41695da301b6eed73bb6294ffd3f3289de32be29
Dariusz Okoński <darek200180@gmail.com>
2024-06-05 18:16:26 +0200

    add submitbtn

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useNavigation } from 'react-router-dom';
 
 const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
-    <button type='submit' className='btn btn-primary'>
-      {text}
+    <button
+      type='submit'
+      className='btn btn-primary btn-block'
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <React.Fragment>
+          <span className='loading loading-spinner'></span>
+          sending...
+        </React.Fragment>
+      ) : (
+        text || 'submit'
+      )}
     </button>
   );
 };
