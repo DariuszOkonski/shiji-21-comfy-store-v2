7630edab4053de8fa1a4137381c56ab47d40f688
Dariusz Okoński <darek200180@gmail.com>
2024-06-06 19:22:33 +0200

    add hero page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import { Hero } from '../components';
+
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <React.Fragment>
+      <Hero />
+    </React.Fragment>
   );
 };
 
