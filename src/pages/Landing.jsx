42f437ccd54aefc68895096f1c1be7bcf067810c
Dariusz Okoński <darek200180@gmail.com>
2024-06-08 11:01:01 +0200

    add loader to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { Hero } from '../components';
+import { customFetch } from '../utils/index';
+
+const url = '/products?featured=true';
+
+export const loader = async () => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  return { products };
+};
 
 const Landing = () => {
   return (
