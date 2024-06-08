fe01404a7c8c0eb4d275181dd341e17c343f3ae2
Dariusz Okoński <darek200180@gmail.com>
2024-06-08 11:42:33 +0200

    fetch single product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,17 @@
+import { useLoaderData } from 'react-router-dom';
+import { customFetch, formatPrice } from '../utils';
+
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+  return { product: response.data.data };
+};
+
 const SingleProduct = () => {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatPrice(price);
+
   return (
     <div>
       <h1>SingleProduct</h1>
