c43b299a3aa88544afdede6f3583c45496d24bca
Dariusz Okoński <darek200180@gmail.com>
2024-06-12 18:35:44 +0200

    add products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,26 @@
+import React from 'react';
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+import { useLoaderData } from 'react-router-dom';
+
+const url = '/products';
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  const meta = response.data.meta;
+
+  return { products, meta };
+};
+
 const Products = () => {
+  const { products, meta } = useLoaderData();
+
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <React.Fragment>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </React.Fragment>
   );
 };
 
