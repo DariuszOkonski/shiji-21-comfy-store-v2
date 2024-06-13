360b04461d3424dd056860e23a78416a578fc5e7
Dariusz Okoński <darek200180@gmail.com>
2024-06-13 18:09:50 +0200

    add base filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Form, Link, useLoaderData } from 'react-router-dom';
+import FormInput from './FormInput';
 
 const Filters = () => {
   return (
-    <div>
-      <h1>Filters</h1>
-    </div>
+    <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* SEARCH */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        size='input-sm'
+      />
+
+      {/* BUTTONS */}
+      <button type='submit' className='btn btn-primary btn-sm'>
+        search
+      </button>
+      <Link to='/products' className='btn btn-accent btn-sm'>
+        reset
+      </Link>
+    </Form>
   );
 };
 
