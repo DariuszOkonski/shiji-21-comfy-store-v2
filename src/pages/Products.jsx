8f8cab1dfefb771086151a44bc2e8b44ce3cf871
Dariusz Okoński <darek200180@gmail.com>
2024-06-17 18:32:38 +0200

    add filters functionality

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,11 +5,15 @@ import { useLoaderData } from 'react-router-dom';
 
 const url = '/products';
 export const loader = async ({ request }) => {
-  const response = await customFetch(url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+
+  const response = await customFetch(url, { params });
   const products = response.data.data;
   const meta = response.data.meta;
 
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => {
