96331e512f2c51a22619920054d51a29b2450e78
Dariusz Okoński <darek200180@gmail.com>
2024-06-26 21:00:40 +0200

    add error

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -44,6 +44,10 @@ export const action =
         error?.response?.data?.error?.message ||
         'there was an error placing your order';
       toast.error(errorMessage);
+
+      if (error.response.status === 401) {
+        return redirect('/login');
+      }
       return null;
     }
   };
